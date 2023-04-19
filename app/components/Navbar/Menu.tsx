5ac182c996b65bb007b62c199fc07c728bb6417b
Pedro Martinez <104749449+PedroMtz8@users.noreply.github.com>
2023-04-18 23:36:52 -0600

    Navbar almost completed

---
+++ app/components/Navbar/Menu.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+interface MenuItemProps {
+    onClick: () => void;
+    label: string
+}
+
+function MenuItem({onClick, label}: MenuItemProps){
+    return(
+        <div
+            onClick={onClick}
+            className="
+                px-4
+                py-3
+                hover:bg-white-100
+                transition
+                font-semibold
+            "
+        >
+            {label}
+
+        </div>
+    )
+}
+
+export default MenuItem;
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 export default function Navbar() {
     return (
@@ -20,6 +21,7 @@ export default function Navbar() {
                     >
                         <Logo />
                         <Search />
+                        <UserMenu />
 
                     </div>
                 </Container>
