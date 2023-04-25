3c7a83dad96cf2bd92e5904d08a2a915e805c8d8
Pedro Martinez <104749449+PedroMtz8@users.noreply.github.com>
2023-04-25 00:00:31 -0600

    interface

--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -1,4 +1,15 @@
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form"
 
+interface InputProps {
+    id: string
+    label: string
+    type?: string
+    disabled?: boolean
+    formatPrice?: boolean
+    required?: boolean
+    register: UseFormRegister<FieldValues>
+    errors: FieldErrors
+}
 
 function Input() {
     return (
