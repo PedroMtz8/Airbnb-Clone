4dd553a6234d0bf851e6b7b9bcd905df59c7c9c5
Pedro Martinez <104749449+PedroMtz8@users.noreply.github.com>
2023-04-24 23:58:45 -0600

    update 2

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -3,9 +3,10 @@ import { AiOutlineMenu } from "react-icons/ai"
 import Avatar from "../Avatar"
 import MenuItem from "./MenuItem"
 import { useCallback, useState } from "react"
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 function UserMenu() {
-
+    const registerModal = useRegisterModal()
     const [isOpen, setIsOpen] = useState(false)
 
     const toggleMenu = useCallback(() => {
@@ -73,7 +74,7 @@ function UserMenu() {
                         <div className="flex flex-col cursor-pointer" >
                             <>
                                 <MenuItem onClick={() => { }} label="Login" />
-                                <MenuItem onClick={() => { }} label="Sign Up" />                            
+                                <MenuItem onClick={registerModal.onOpen} label="Sign Up" />                            
                             </>
                         </div>
                     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import ClientOnly from './components/ClientOnly'
 import Navbar from './components/Navbar/Navbar'
-import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
 import './globals.css'
 import { Nunito } from 'next/font/google'
 
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className} >
         <ClientOnly>
-          <Modal actionLabel='Submit' isOpen title='Login to your account' />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
