4a180fc1c0753b38dc343ca6c20cd264cc2d97ac
Pedro Martinez <104749449+PedroMtz8@users.noreply.github.com>
2023-05-02 22:15:21 -0600

    removes 2

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,9 +1,6 @@
 import { User } from "@prisma/client";
 
-export type SaveUser = Omit<
-  User,
-  "createdAt" | "updatedAt" | "emailVerified"
-> & {
+export type SaveUser = Omit<User, "createdAt" | "updatedAt" | "emailVerified"> & {
   createdAt: string;
   updatedAt: string;
   emailVerified: string | null;
