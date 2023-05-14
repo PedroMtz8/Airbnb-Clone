e05de8bd87c861b066bc986855aa5437c5447b9e
Pedro Martinez <104749449+PedroMtz8@users.noreply.github.com>
2023-05-13 20:41:46 -0600

    rent modal form initialized

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -5,6 +5,7 @@ import { useMemo, useState } from "react";
 import Heading from "../Heading";
 import { categories } from "../Navbar/Categories";
 import CategoryInput from "../Input/CategoryInput";
+import { FieldValues, useForm } from "react-hook-form";
 
 enum STEPS {
     CATEGORY = 0,
@@ -18,6 +19,39 @@ enum STEPS {
 function RentModal() {
     const rentModal = useRentModal()
     const [step, setStep] = useState(STEPS.CATEGORY);
+
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        watch,
+        formState: {
+            errors
+        },
+        reset
+    } = useForm<FieldValues>({
+        defaultValues: {
+            category: '',
+            location: null,
+            guestCount: 1,
+            roomCount: 1,
+            bathroomCount: 1,
+            imageSrc: '',
+            price: 1,
+            title: "",
+            description: ""
+        }
+    });
+
+    const category = watch('category');
+
+    const setCustomValue = (id: string, value: any) => {
+        setValue(id, value, {
+            shouldDirty: true,
+            shouldTouch: true,
+            shouldValidate: true
+        })
+    }
 
     const onBack = () => {
         setStep((value) => value - 1)
@@ -62,8 +96,8 @@ function RentModal() {
                     categories.map((item) => (
                         <div key={item.label} className="col-span-1" >
                             <CategoryInput
-                                onClick={() => { }}
-                                selected={false}
+                                onClick={(category) => setCustomValue('category', category)}
+                                selected={category === item.label}
                                 label={item.label}
                                 icon={item.icon}
                             />
