6be9a51c5e7babeb94f2b94d557de4cbc75b9fc0
Pedro Martinez <pedrocmartinez568@gmail.com>
2023-06-24 14:29:25 -0600

    ssr listingId

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -4,15 +4,36 @@ import ClientOnly from '@/app/components/ClientOnly';
 import EmptyState from '@/app/components/EmptyState';
 import ListingClient from './listingClient';
 import getReservations from '@/app/actions/getReservations';
+import type { Metadata } from 'next';
+import { Listing } from '@prisma/client';
+import { SafeListing } from '@/app/types';
 
 interface IParams {
   listingId?: string
+}
+type Params = {
+  params: {
+    listingId: string;
+  }
+}
+
+export async function generateMetadata({params: { listingId } }: Params){
+  const listing = await getListingById({ listingId });
+  return {
+    title: listing?.title + " | Airbnb",
+    description: listing?.description,
+    // icons: {
+    //   icon: listing?.imageSrc
+    // },
+  }
+
 }
 
 async function ListingPage({ params }: { params: IParams }) {
   const listing = await getListingById(params);
   const currentUser = await getCurrentUser();
   const reservations = await getReservations(params);
+  // console.log(listing.)
 
   if (!listing) return (
     <ClientOnly>
