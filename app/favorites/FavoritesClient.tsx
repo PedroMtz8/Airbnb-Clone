3b2685517bf933da0b19bb87e1b6669bcfa9841f
Pedro Martinez <104749449+PedroMtz8@users.noreply.github.com>
2023-07-18 23:39:02 -0600

    display favorites

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import Container from '../components/Container';
+import Heading from '../components/Heading';
+import ListingCard from '../components/Listings/ListingCard';
 import { SafeListing, SaveUser } from '../types';
 
 interface FavoritesClientProps {
@@ -9,7 +12,37 @@ interface FavoritesClientProps {
 
 function FavoritesClient({listings, currentUser}: FavoritesClientProps){
   return(
-    <div>Here will be favorites listing</div>
+    <Container>
+      <Heading
+        title='Favorites'
+        subtitle='List of places you have favorited!'
+      />
+      <div
+        className="
+          mt-10
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {
+          listings?.map((listing, i) => (
+            <ListingCard 
+              key={listing.id}
+              currentUser={currentUser}
+              data={listing}
+
+            />
+          ))
+        }
+
+      </div>
+    </Container>
   )
 }
 
